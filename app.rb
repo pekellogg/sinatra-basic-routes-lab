da7a89c1306387b747c98ba60a094a7e05370e67
Peyton <pekellogg@hotmail.com>
2022-03-13 22:46:08 -0700

    complete

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Peyton"
+    end
+    
+    get '/hometown' do 
+        "My hometown is Seattle"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is Get Into It (yuh)"
+    end
+
 end
